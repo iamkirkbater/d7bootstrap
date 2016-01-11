ac55ba9c66c7e53a899000eea402e6f9c1478130
Kirk Bater <kxbtwc@rit.edu>
2016-01-11 11:21:23 -0500

    Added some comments to the banner component.

--- templates/components/banner.php
+++ templates/components/banner.php
@@ -2,16 +2,27 @@
     <div class="container">
         <?php if (count($banners) > 1): ?>
             <div class="banner banner--slideshow">
-                <?php shuffle($banners); ?>
+                <?php
+
+                /**
+                 * If the banners should not be random, comment out this line.
+                 */
+                shuffle($banners);
+
+
+                /**
+                 * Note: If you do not set up an Image Style called "Banner Image" this will fail to work properly.
+                 */
+                ?>
                 <?php foreach ($banners as $banner) : ?>
                     <div>
-                        <img src="<?php print image_style_url('featured_story_banner', $banner['uri']); ?>" alt="<?php print $title; ?>">
+                        <img src="<?php print image_style_url('banner_image', $banner['uri']); ?>" alt="<?php print $title; ?>">
                     </div>
                 <?php endforeach; ?>
             </div>
         <?php else: ?>
             <div class="banner banner--single">
-                <img src="<?php print image_style_url('featured_story_banner', $banners[0]['uri']); ?>" alt="<?php print $title; ?>">
+                <img src="<?php print image_style_url('banner_image', $banners[0]['uri']); ?>" alt="<?php print $title; ?>">
             </div>
         <?php endif; ?>
     </div>
